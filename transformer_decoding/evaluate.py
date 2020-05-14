3097611269688eb4f17cf6decec62cf96c33ac0e
Chris Hokamp <chris.hokamp@gmail.com>
2020-05-14 11:45:28 +0100

    commit before simplifying lebanoff et al eval logic

--- transformer_decoding/evaluate.py
+++ transformer_decoding/evaluate.py
@@ -2,10 +2,15 @@ import argparse
 import json
 from pathlib import Path
 import tqdm
+import os
 from _collections import defaultdict
 
 import numpy as np
 import torch
+import spacy
+
+import pyrouge
+import logging
 
 from transformers import (modeling_utils,
                           BartTokenizer,
@@ -18,6 +23,144 @@ import transformer_decoding.log as log
 from newsroom.analyze.rouge import ROUGE_L, ROUGE_N
 
 logger = log.create_logger(__name__)
+
+
+# BEGIN: utils for Lebanoff 2018 rouge
+def make_html_safe(s):
+    """Replace any angled brackets in string s to avoid interfering with HTML attention visualizer."""
+    s.replace("<", "&lt;")
+    s.replace(">", "&gt;")
+    return s
+
+
+def rouge_eval(ref_dir, dec_dir):
+    """Evaluate the files in ref_dir and dec_dir with pyrouge, returning results_dict"""
+    r = pyrouge.Rouge155()
+    #   r.model_filename_pattern = '#ID#_reference.txt'
+    r.model_filename_pattern = '#ID#_reference.[A-Z].txt'
+    r.system_filename_pattern = '(\d+)_decoded.txt'
+    r.model_dir = ref_dir
+    r.system_dir = dec_dir
+    logging.getLogger('global').setLevel(logging.WARNING)  # silence pyrouge logging
+    rouge_args = ['-e', r._data_dir,
+                  '-c',
+                  '95',
+                  '-2', '4',  # This is the only one we changed (changed the max skip from -1 to 4)
+                  '-U',
+                  '-r', '1000',
+                  '-n', '4',
+                  '-w', '1.2',
+                  '-a',
+                  '-l', '100']
+    rouge_args = ' '.join(rouge_args)
+    rouge_results = r.convert_and_evaluate(rouge_args=rouge_args)
+    return r.output_to_dict(rouge_results)
+
+
+# def rouge_log(results_dict, dir_to_write):
+def rouge_log(results_dict):
+    """Log ROUGE results to screen and write to file.
+    Args:
+        results_dict: the dictionary returned by pyrouge
+        dir_to_write: the directory where we will write the results to"""
+    log_str = ""
+    for x in ["1", "2", "l", "s4", "su4"]:
+        log_str += "\nROUGE-%s:\n" % x
+        for y in ["f_score", "recall", "precision"]:
+            key = "rouge_%s_%s" % (x, y)
+            key_cb = key + "_cb"
+            key_ce = key + "_ce"
+            val = results_dict[key]
+            val_cb = results_dict[key_cb]
+            val_ce = results_dict[key_ce]
+            log_str += "%s: %.4f with confidence interval (%.4f, %.4f)\n" % (key, val, val_cb, val_ce)
+    logging.info(log_str)  # log to screen
+    #     results_file = os.path.join(dir_to_write, "ROUGE_results.txt")
+    #     print("Writing final ROUGE results to %s...", results_file)
+    #     with open(results_file, "w") as f:
+    #         f.write(log_str)
+    return log_str
+
+
+def write_for_rouge(all_reference_sents, decoded_words, ex_index, rouge_dec_dir, rouge_ref_dir, nlp):
+    """Write output to file in correct format for eval with pyrouge. This is called in single_pass mode.
+    Args:
+        all_reference_sents: list of list of strings
+        decoded_words: list of strings
+        ex_index: int, the index with which to label the files
+    """
+
+    # First, divide decoded output into sentences
+    decoded_sents = []
+    while len(decoded_words) > 0:
+        try:
+            fst_period_idx = decoded_words.index(".")
+        except ValueError:  # there is text remaining that doesn't end in "."
+            fst_period_idx = len(decoded_words)
+        sent = decoded_words[:fst_period_idx + 1]  # sentence up to and including the period
+        decoded_words = decoded_words[fst_period_idx + 1:]  # everything else
+        decoded_sents.append(' '.join(sent))
+
+    # pyrouge calls a perl script that puts the data into HTML files.
+    # Therefore we need to make our output HTML safe.
+    decoded_sents = [make_html_safe(w) for w in decoded_sents]
+    # note sentence splitting here
+    all_reference_sents = [
+        [make_html_safe(' '.join([str(w) for w in s])) for s in nlp(abstract).sents]
+        for abstract in all_reference_sents
+    ]
+
+    # Write to file
+    decoded_file = os.path.join(rouge_dec_dir, "%06d_decoded.txt" % ex_index)
+
+    for abs_idx, abs in enumerate(all_reference_sents):
+        ref_file = os.path.join(rouge_ref_dir, "%06d_reference.%s.txt" % (
+            ex_index, chr(ord('A') + abs_idx)))
+        with open(ref_file, "w") as f:
+            # one long line
+            # f.write(' '.join(abs).lower() + '\n')
+
+            # one sentence on each line
+            for idx, sent in enumerate(abs):
+                f.write(sent + "\n")
+
+                # f.write(sent) if idx==len(abs)-1 else f.write(sent+"\n")
+    with open(decoded_file, "w") as f:
+        # one long line
+        # f.write(' '.join(decoded_sents).lower() + '\n')
+        for idx, sent in enumerate(decoded_sents):
+            f.write(sent + "\n")
+
+
+# TODO: WORKING: support both newsroom and lebanoff et al eval within same function
+def lebanoff_2018_rouge(system_hyp_file, evaluation_dataset):
+    TEMP_EVAL_DIR = Path('rouge_evaluation_tempdir')
+    rouge_dec_dir = TEMP_EVAL_DIR / 'rouge_dec_dir'
+    rouge_ref_dir = TEMP_EVAL_DIR / 'rouge_ref_dir'
+    rouge_dec_dir.mkdir(parents=True, exist_ok=True)
+    rouge_ref_dir.mkdir(parents=True, exist_ok=True)
+
+    nlp = spacy.load("en_core_web_sm")
+
+    # dataset needs to be in .jsonl
+    dataset_rows = [json.loads(l) for l in open(evaluation_dataset)]
+
+    # tokenize hyps to follow Lebanoff et al 2018 logic
+    system_hyp_tokens = [[str(t) for t in nlp(h.strip())] for h in open(system_hyp_file)]
+
+    # write the rouge files
+    for idx, (row, h) in enumerate(zip(dataset_rows, system_hyp_tokens)):
+        if type(row['summary']) is list:
+            summaries = row['summary']
+        else:
+            summaries = [row['summary']]
+        #     print(f'{len(summaries)} summaries available at row {idx}')
+        write_for_rouge(summaries, h, idx, rouge_dec_dir, rouge_ref_dir, nlp)
+
+    log_report = rouge_log(rouge_eval(rouge_ref_dir, rouge_dec_dir))
+    print(log_report)
+
+# END: utils for Lebanoff 2018 rouge
 
 
 def print_mean(results, rouge_types):
@@ -158,6 +301,7 @@ def main(args):
     # load pretrained or finetuned transformer model
     print(f'loading pre-trained model: {args["model_id"]}')
 
+    # we have to load fine-tuned models in a different way because of pytorch-lightning
     # fine-tuned
     if args['model_id'].endswith('.ckpt'):
     	from transformer_decoding.finetune import SummarizationTrainer
@@ -173,15 +317,12 @@ def main(args):
     # This is IMPORTANT to have reproducible results during evaluation!
     args['model'].eval()
 
-    # WORKING: we have to load fine-tuned models in a different way because of pytorch-lightning
-
     if torch.cuda.is_available():
         args['model'].to('cuda')
 
     # summarize MDS / summarization dataset with model
 
     # print and write out evaluation results
-    # TODO: WORKING: in general we want to be able to ensemble both models _and_ inputs
     if args['evaluation_dataset'].endswith('.jsonl'):
         dataset = [json.loads(l) for l in open(args['evaluation_dataset'])][:args['rows_to_eval']]
     else:
@@ -218,12 +359,15 @@ def main(args):
     preds_output.close()
     gold_output.close()
 
+    # Evaluation
     hyps, refs = zip(*summaries)
     results, rouge_types = evaluate_rouge(hyps, refs)
 
     print_mean(results, rouge_types)
+    # End evaluation
 
 
+# TODO: one arg set for evaluation on files, one for predicting, writing files, and evaluating with model
 def parse_args():
     parser = argparse.ArgumentParser()
     #parser.add_argument(
