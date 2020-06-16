ce6a7b2ba8a9f79d466115903b7bf20dfba2eb6b
Chris Hokamp <chris.hokamp@gmail.com>
2020-06-16 22:08:02 +0000

    consolidating evaluation logic

--- transformer_decoding/evaluate.py
+++ transformer_decoding/evaluate.py
@@ -30,6 +30,11 @@ np.random.seed(42)
 
 
 # BEGIN: utils for Lebanoff 2018 rouge
+# adapted from here: 
+# https://github.com/ucfnlp/multidoc_summarization/blob/ae30c9ee039d4ad5ff64fd2245faafc5a62c4dd7/decode.py
+
+# installing pyrouge
+# https://stackoverflow.com/questions/45894212/installing-pyrouge-gets-error-in-ubuntu
 def make_html_safe(s):
     """Replace any angled brackets in string s to avoid interfering with HTML attention visualizer."""
     s.replace("<", "&lt;")
@@ -61,7 +66,6 @@ def rouge_eval(ref_dir, dec_dir):
     return r.output_to_dict(rouge_results)
 
 
-# def rouge_log(results_dict, dir_to_write):
 def rouge_log(results_dict):
     """Log ROUGE results to screen and write to file.
     Args:
@@ -79,10 +83,6 @@ def rouge_log(results_dict):
             val_ce = results_dict[key_ce]
             log_str += "%s: %.4f with confidence interval (%.4f, %.4f)\n" % (key, val, val_cb, val_ce)
     logging.info(log_str)  # log to screen
-    #     results_file = os.path.join(dir_to_write, "ROUGE_results.txt")
-    #     print("Writing final ROUGE results to %s...", results_file)
-    #     with open(results_file, "w") as f:
-    #         f.write(log_str)
     return log_str
 
 
@@ -165,6 +165,58 @@ def lebanoff_2018_rouge(system_hyp_file, evaluation_dataset):
     shutil.rmtree(TEMP_EVAL_DIR)
 
 # END: utils for Lebanoff 2018 rouge
+
+# BEGIN: utils for Fabbri 2019 rouge
+# Evaluation from: https://github.com/Alex-Fabbri/Multi-News/blob/3675e7c422ae3b4020617a324ac264f50333357d/code/OpenNMT-py-baselines/tools/test_rouge.py
+def test_rouge(candidates, references):
+    """Calculate ROUGE scores of sequences passed as an iterator
+       e.g. a list of str, an open file, StringIO or even sys.stdin
+    """
+    current_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+    tmp_dir = ".rouge-tmp-{}".format(current_time)
+    try:
+        if not os.path.isdir(tmp_dir):
+            os.mkdir(tmp_dir)
+            os.mkdir(tmp_dir + "/candidate")
+            os.mkdir(tmp_dir + "/reference")
+#         candidates = [line.strip() for line in cand]
+#         references = [line.strip() for line in ref]
+        assert len(candidates) == len(references)
+        cnt = len(candidates)
+        for i in range(cnt):
+            if len(references[i]) < 1:
+                continue
+            with open(tmp_dir + "/candidate/cand.{}.txt".format(i), "w",
+                      encoding="utf-8") as f:
+                f.write(candidates[i])
+            with open(tmp_dir + "/reference/ref.{}.txt".format(i), "w",
+                      encoding="utf-8") as f:
+                f.write(references[i])
+        r = pyrouge.Rouge155()
+        r.model_dir = tmp_dir + "/reference/"
+        r.system_dir = tmp_dir + "/candidate/"
+        r.model_filename_pattern = 'ref.#ID#.txt'
+        r.system_filename_pattern = 'cand.(\d+).txt'
+        rouge_results = r.convert_and_evaluate()
+        results_dict = r.output_to_dict(rouge_results)
+        return results_dict
+    finally:
+        pass
+        if os.path.isdir(tmp_dir):
+            shutil.rmtree(tmp_dir)
+
+
+def rouge_results_to_str(results_dict):
+    return ">> ROUGE(1/2/3/L/SU4): {:.2f}/{:.2f}/{:.2f}/{:.2f}/{:.2f}".format(
+        results_dict["rouge_1_f_score"] * 100,
+        results_dict["rouge_2_f_score"] * 100,
+        results_dict["rouge_3_f_score"] * 100,
+        results_dict["rouge_l_f_score"] * 100,
+        results_dict["rouge_su*_f_score"] * 100)
+
+
+
+# END: utils for Fabbri 2019 rouge
 
 
 def print_mean(results, rouge_types):
